93fa49c89fd998d28b6df1004a79a7d05e5f07ae
Roy McElmurry <shakalandro@gmail.com>
2012-01-27 20:04:20 -0800

    boilerplate

--- metagame.js
+++ metagame.js
@@ -1,1 +1,15 @@
-alert('hello world');
+/*
+Roy McElmurry, Erik Nelson, Eric Spishak
+MetaGame
+UW hangout Hangout Hackathon, Winter 2012
+*/
+
+gapi.hangout.onApiReady.add(function(eventObj){
+	if (eventObj.isApiReady) {
+	    startApp();
+	}
+});
+
+function startApp() {
+    $('#app_body').append($('<p>Hello World</p>'));
+}
