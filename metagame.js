8bbf5051fadb97ce74fda3764685a9d749e6f936
Roy McElmurry <shakalandro@gmail.com>
2012-01-27 22:19:35 -0800

    get games list

--- metagame.js
+++ metagame.js
@@ -4,6 +4,9 @@ MetaGame
 UW hangout Hangout Hackathon, Winter 2012
 */
 
+
+var MOCHI_GAME_SERVICE = 'http://catalog.mochimedia.com/feeds/query/';
+
 gapi.hangout.onApiReady.add(function(eventObj){
 	if (eventObj.isApiReady) {
 	    startApp();
@@ -12,4 +15,34 @@ gapi.hangout.onApiReady.add(function(eventObj){
 
 function startApp() {
     $('#app_body').append($('<p>Hello World</p>'));
+    getGames(function(gs) {
+	    console.log(gs);
+    });
+}
+
+
+/*
+Calls cb with a list of lists in the following format:
+
+[
+   [name, swf_url]
+]
+*/
+function getGames(cb) {
+    $.ajax({
+	    'url': MOCHI_GAME_SERVICE,
+	    'data': {'q': 'leaderboard_enabled', 'limit': '100'},
+	    'dataType': 'jsonp',
+       	    'success': function(data, textStatus, crap) {
+		var res = [];
+		$.each(data.games, function(idx, value) {
+			res.push([value.name, value.swf_url]);
+		    }
+		);
+		cb(res);
+	    },
+	    'error': function() {
+		// Catch failure
+	    } 
+    });
 }
